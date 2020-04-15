49b00840f264cac14979e3e7665bc4ec0afa433c
jay51 <nenojaafar3@gmail.com>
2020-04-15 01:34:50 -0500

    simple test cases

--- examples/main.c
+++ examples/main.c
@@ -6,33 +6,31 @@
 
 
 int main(){
-    // TODO: find the size of hashmap function
-    // find if hashmap is empty
-    // write test for hashmap
 
+    char* keys[] = {"testkey0", "testkey1", "testkey2", 
+                    "testkey3", "testkey4", "testkey5", 
+                    "testkey6", "testkey7", "testkey8", 
+                    '\0'};
+
+    size_t n = sizeof(keys)/sizeof(keys[0]) - 1;
 
     map_str_t hashmap;
     map_init(&hashmap);
 
-    map_set(&hashmap, "testkey0", "testvalue");
-    map_set(&hashmap, "testkey1", "testvalue");
-    map_set(&hashmap, "testkey2", "testvalue");
-    // map_set(&hashmap, "testkey3", "testvalue");
-    // map_set(&hashmap, "testkey4", "testvalue");
-    // map_set(&hashmap, "testkey5", "testvalue");
-    // map_set(&hashmap, "testkey6", "testvalue");
-    // map_set(&hashmap, "testkey7", "testvalue");
-    // map_set(&hashmap, "testkey8", "testvalue");
+    for(int i =0; i < n; i++){
+        map_set(&hashmap, keys[i], keys[i]);
+    }
 
     printf("size %d\n", map_size(&hashmap));
-
     // printf("nnodes: %d\n", hashmap.base.nnodes); // number of key, value
     // printf("nbuckets: %d\n", hashmap.base.nbuckets); // power of 2 buckets to hold nodes
 
-    char** val = map_get(&hashmap, "testkey");
-    if (val) {
-        printf("value: %s\n", *val);
-    } else {
-        printf("value not found\n");
+    for(int i =0; i < n; i++){
+        char** val = map_get(&hashmap, keys[i]);
+        if (val) {
+            printf("value: %s\n", *val);
+        } else {
+            printf("value not found\n");
+        }
     }
 }
