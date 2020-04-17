fe5c087d7429063e169d8ff5d879c88c31bef561
jay51 <nenojaafar3@gmail.com>
2020-04-16 21:37:49 -0500

    improve test

--- examples/main.c
+++ examples/main.c
@@ -1,5 +1,7 @@
 
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "map.h"
 
 
@@ -7,18 +9,17 @@
 
 int main(){
 
-    char* keys[] = {"testkey0", "testkey1", "testkey2", 
-                    "testkey3", "testkey4", "testkey5", 
-                    "testkey6", "testkey7", "testkey8", 
-                    '\0'};
-
-    size_t n = sizeof(keys)/sizeof(keys[0]) - 1;
+    int n = 10000;
 
     map_str_t hashmap;
     map_init(&hashmap);
 
     for(int i =0; i < n; i++){
-        map_set(&hashmap, keys[i], keys[i]);
+        char buf[245];
+        char* key_val = "testkey_val";
+        sprintf(buf, "%s%d", key_val, i);
+        // printf(buf);
+        map_set(&hashmap, buf, buf);
     }
 
     printf("size %d\n", map_size(&hashmap));
@@ -26,9 +27,22 @@ int main(){
     // printf("nbuckets: %d\n", hashmap.base.nbuckets); // power of 2 buckets to hold nodes
 
     for(int i =0; i < n; i++){
-        char** val = map_get(&hashmap, keys[i]);
+        sleep(0.9);
+        char buf[245];
+        char* key_val = "testkey_val";
+        sprintf(buf, "%s%d", key_val, i);
+        char** val = map_get(&hashmap, buf);
+
         if (val) {
-            printf("value: %s\n", *val);
+            if((strcmp(buf, *val)) == 0){
+                printf("\033[0m");
+                printf("correct -> %s\n", *val);
+
+            } else{
+                printf("\033[0;31m");
+                printf("wrong ->%s\n", *val);
+            }
+
         } else {
             printf("value not found\n");
         }
