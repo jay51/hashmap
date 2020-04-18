f43296f5407dc3c988eed7b3d7d9bae2ddbde4ac
jay51 <nenojaafar3@gmail.com>
2020-04-18 01:30:58 -0500

    add test

--- examples/main.c
+++ examples/main.c
@@ -6,10 +6,87 @@
 
 
 
+void test_with_many_values(int n);
+void test_remove();
+void test_replace();
+void test_adding();
 
 int main(){
+    test_with_many_values(10);
+    test_remove();
+    test_replace();
+    test_adding();
 
-    int n = 10000;
+}
+
+
+void test_adding(){
+    map_int_t hashmap;
+    map_init(&hashmap);
+
+    char* key1 = "Ea";
+    char* key2 = "FB";
+
+    map_set(&hashmap, key1, 1);
+    printf("key1 is %d, AND SHOULD BE 1\n", *map_get(&hashmap, key1));
+
+    map_set(&hashmap, key2, 2);
+    printf("key2 is %d, AND SHOULD BE 2\n", *map_get(&hashmap, key2));
+
+    map_deinit(&hashmap);
+}
+
+
+void test_replace(){
+
+    map_int_t hashmap;
+    map_init(&hashmap);
+
+    char* key = "Hello";
+
+    map_set(&hashmap, key, 1);
+    printf("key is %d, AND SHOULD BE 1\n", *map_get(&hashmap, key));
+
+    map_set(&hashmap, key, 2);
+    printf("key is %d, AND SHOULD BE 2\n", *map_get(&hashmap, key));
+
+    map_deinit(&hashmap);
+}
+
+
+void test_remove(){
+
+    map_int_t hashmap;
+    map_init(&hashmap);
+
+    char* key1 = "Hello";
+    char* key2 = "Goodbye";
+    char* key3 = "new";
+
+    map_set(&hashmap, key1, 1);
+    printf("ADDING KEY1, SIZE IS %d AND SHOULD BE 1\n", map_size(&hashmap));
+
+    map_set(&hashmap, key2, 2);
+    printf("ADDING KEY2, SIZE IS %d AND SHOULD BE 2\n", map_size(&hashmap));
+
+    map_remove(&hashmap, key1);
+    printf("REMOVE KEY1, SIZE IS %d AND SHOULD BE 1\n", map_size(&hashmap));
+
+    printf("key2: %d\n", *map_get(&hashmap, key2));
+
+    map_remove(&hashmap, key2);
+    printf("REMOVE KEY2, SIZE IS %d AND SHOULD BE 0\n", map_size(&hashmap));
+
+    map_set(&hashmap, key3, 3);
+    printf("ADDING KEY3, SIZE IS %d AND SHOULD BE 1\n", map_size(&hashmap));
+    printf("key3: %d\n", *map_get(&hashmap, key3));
+
+    map_deinit(&hashmap);
+}
+
+
+
+void test_with_many_values(int n){
 
     map_str_t hashmap;
     map_init(&hashmap);
@@ -37,14 +114,22 @@ int main(){
             if((strcmp(buf, *val)) == 0){
                 printf("\033[0m");
                 printf("correct -> %s\n", *val);
+                map_remove(&hashmap, buf);
 
             } else{
                 printf("\033[0;31m");
                 printf("wrong ->%s\n", *val);
+                break;
             }
 
         } else {
             printf("value not found\n");
+            printf("wrong ->%s\n", buf);
+            break;
         }
     }
+
+    printf("nbuckets: %d\n", hashmap.base.nbuckets); // power of 2 buckets to hold nodes
+    printf("size %d\n", map_size(&hashmap));
+    map_deinit(&hashmap);
 }
